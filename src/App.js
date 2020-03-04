2ddbfacadb0d25da7f0ad1f310337152a92ea6ce
olympia <owojcik7@gmail.com>
2020-03-04 14:06:47 -0500

    Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Canvas from "./Canvas"
+
+function App() {
+
+
+
+  return (
+    <div className="App">
+
+      <p>This is a Canvas </p>
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
